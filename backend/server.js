55ae1494702d89f97e54090edbef906165fdb98a
Swathi0141 <90282618+Swathi0141@users.noreply.github.com>
2024-06-12 14:40:29 -0500

    Completed connecting to database and user setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // to parse incoming req with json payload (from req.body)
+app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+     // root route http://localhost:5000/
+//     res.send("Hello!");
+// });
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)
+});
