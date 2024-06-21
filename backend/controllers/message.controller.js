eaa8dd250220c955c0ca2ad8c08bb9079636d109
Swathi0141 <90282618+Swathi0141@users.noreply.github.com>
2024-06-21 18:12:56 -0500

    Implemented Socket

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
     try {
@@ -26,13 +27,16 @@ export const sendMessage = async (req, res) => {
         if (newMessage) {
             conversation.messages.push(newMessage._id);
         }
+
+         // the above 2 commands are replaced with this. it runs in parallel
+         await Promise.all([conversation.save(), newMessage.save()]);
+
         // SOCKET.IO FUNCTIONALITY GOES HERE to make it realtime
-
-        // await conversation.save();
-        // await newMessage.save();
-
-        // the above 2 commands are replaced with this. it runs in parallel
-        await Promise.all([conversation.save(), newMessage.save()]);
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            // io.to(<socket_id>).emit() used to send events to specific client
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(201).json(newMessage);
     } catch (error) {
